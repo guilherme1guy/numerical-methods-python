8c9ee36a58e5f17d50824629dc4c9546bc6e6fbd
Guilherme Guy <guilhermeguy349@gmail.com>
2021-08-30 20:51:32 -0300

    Adds two methods to zero solutions

--- solutions.py
+++ solutions.py
@@ -55,6 +55,94 @@ def bisection(f, a, b, tol, iter_max):
     return [root, i, converged]
 
 
+def fake_position(f, a, b, tol, iter_max):
+    """
+    Calculates the root of an equation by Fake Position method
+    Parameters:
+        f: Function f(x)
+        a: Lower limit
+        b: Upper limit
+        tol: Tolerance
+        iter_max: Maximum number of iterations
+    Returns:
+        root: Root value
+        iter: Used iterations
+        converged: Found the root
+    """
+
+    fa = f(a)
+    fb = f(b)
+
+    if fa * fb > 0:
+        raise Exception("The function does not change signal at \
+              the ends of the given interval.")
+
+    delta_x = math.fabs(b - a) / 2
+
+    x = 0
+    converged = False
+    i = 0
+
+    for i in range(0, iter_max + 1):
+        x = ((a * fb) - (b * fa)) / (fb - fa)
+        fx = f(x)
+
+        print("i: {:03d}\t x: {:+.4f}\t fx: {:+.4f}\t dx: {:+.4f}\n"
+              .format(i, x, fx, delta_x), end="")
+
+        if delta_x <= tol and math.fabs(fx) <= tol:
+            converged = True
+            break
+
+        if fa * fx > 0:
+            a = x
+            fa = fx
+        else:
+            b = x
+            fb = fx
+
+        delta_x = math.fabs(b - a) / 2
+    else:
+        print("Warning: The method did not converge.")
+
+    root = x
+    return [root, i, converged]
+
+def fixed_point(f, g, x0, tol, iter_max):
+    """
+    Calculates the root of an equation by Fixed Point method
+    Parameters:
+        f: Function f(x)
+        g: Iteraction Function g(x)
+        x0: Initial point
+        tol: Tolerance
+        iter_max: Maximum number of iterations
+    Returns:
+        root: Root value
+        iter: Used iterations
+        converged: Found the root
+    """
+
+    x = x0
+    fx = f(x)
+    converged = False
+
+    for i in range(0, iter_max):
+        
+        print("i: {:03d}\t x{:02d}: {:+.4f}\t fx{:d}: {:+.4f}\t\n"
+            .format(i, i, x, i, fx), end="")
+        
+        if math.fabs(fx) < tol:
+            converged = True
+            break
+
+        x = g(x)
+        fx = f(x)
+
+
+    root = x
+    return [root, i, converged]
+
 def newton(f, df, x0, tol, iter_max):
     """
     Calculates the root of an equation by Newton method
